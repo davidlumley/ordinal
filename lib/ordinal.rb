1697c4ffdfa5e08a573f53b0b687cd5b2a7ebf76
Andrew Gallagher <admin@kivlor.com>
2012-06-07 20:54:50 +1000

    Added functionality to the to_ordinal method.

--- lib/ordinal.rb
+++ lib/ordinal.rb
@@ -3,6 +3,23 @@ require "ordinal/version"
 module Ordinal
   
   def to_ordinal
+    
+    number = self.to_i
+
+    case
+    when number == 0
+      0.to_s
+    when (number % 100 >= 11 and
+          number % 100 <= 13)
+      number.to_s + 'th'
+    else
+      number.to_s << suffixes[number % 10]
+    end
+    
+  end
+  
+  def suffixes
+    ['th','st','nd','rd','th','th','th','th','th','th']
   end
   
 end
